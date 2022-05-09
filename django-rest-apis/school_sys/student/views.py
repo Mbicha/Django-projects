c55cc79b06710b53784304f8a4d0bbcd40bd6085
Mbicha <[mbithicharlse@gmail.com]>
2022-05-09 11:44:02 +0300

    Added status to respont to responses

--- django-rest-apis/school_sys/student/views.py
+++ django-rest-apis/school_sys/student/views.py
@@ -20,14 +20,18 @@ def createStudent(request):
         seriralizer.save()
         return Response(seriralizer.data)
     else:
-        return Response(seriralizer._errors)
+        return Response(seriralizer._errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def getStudentById(request, pk):
     try:
         student = Student.objects.get(pk=pk)
     except:
-        print("Object not found")
+        return Response(
+            {
+                'errors': "Student doesn't exist"
+            }, status=status.HTTP_404_NOT_FOUND
+        )
 
     if request.method == 'GET':
         serializer = StudentSerializers(student)
@@ -38,10 +42,8 @@ def getStudentById(request, pk):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return serializer.errors
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == 'DELETE':
         student.delete()
-        return Response({
-            'delete': True
-        })
+        return Response(status=status.HTTP_204_NO_CONTENT)
